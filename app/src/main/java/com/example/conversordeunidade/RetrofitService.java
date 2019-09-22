7c89553a43403d505532a430c79d5d961082d4ea
Luis Eduardo <luiseos@outlook.com.br>
2019-09-22 15:52:48 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/conversordeunidade/RetrofitService.java
@@ -0,0 +1,17 @@
+package com.example.conversordeunidade;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface RetrofitService {
+    @Headers("X-Mashape-Key: AuuyclCPjcmshv2iOPq190OpzLrMp1FJWwejsnJrdfwOUr4h44")
+
+    @FormUrlEncoded
+    @POST("convert")
+    Call<RespostaServidor> converterUnidade(@Field("from-type") String from_type,
+                                            @Field("from-value") String from_value,
+                                            @Field("to-type") String to_type);
+}
